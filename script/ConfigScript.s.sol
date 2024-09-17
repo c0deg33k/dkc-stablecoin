f1fc136f8550a9acac9a72f14306ac435846e947
c0deg33k <clinch701@gmail.com>
2024-09-18 01:40:31 +0300

    modified ERC20Mock

--- script/ConfigScript.s.sol
+++ script/ConfigScript.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.22;
 
 import {Script} from "lib/forge-std";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "../test/mocks/ERC20Mock.sol";
 
 contract ConfigScript is Script {
     struct NetworkConfig {
@@ -13,7 +14,11 @@ contract ConfigScript is Script {
         uint256 deployerKey;
     }
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 2000e8;
     NetworkConfig public activeNetworkConfig;
+    
 
     constructor() {}
 
@@ -34,5 +39,13 @@ contract ConfigScript is Script {
         if (activeNetworkConfig.wethUsdPriceFeed != address(0)) {
             return activeNetworkConfig;
         }
+        vm.stopBroadcast();
+        MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
+        ERC20Mock wETHMock = new ERC20Mock("WETH", "WETH", msg.sender, 1000e8);
+
+        MockV3Aggregator btcUsdPriceFeed = new MockV3Aggregator(DECIMALS, BTC_USD_PRICE);
+        ERC20Mock wBTCMock = new ERC20Mock("WBTC", "WBTC", msg.sender, 1000e8);
+
+        vm.stopBroadcast();
     }
 }
